37ce55424ffe115e554fec331fab3eba71f8925d
Wanderson Duarte Alves <wanderson@stacasa.com.br>
2021-08-08 18:38:04 -0300

    Atualizaçoes

--- database/database.js
+++ database/database.js
@@ -4,14 +4,16 @@ const localhost = {
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : "db_metas_sta"
+  database : "db_metas_sta",
+  multipleStatements: true
 };
 
 const sta_casa = {
   host     : '10.10.10.5',
   user     : 'root',
   password : 'controle',
-  database : "db_metas_sta"
+  database : "db_metas_sta",
+  multipleStatements: true
 };
 
 var connection = mysql.createConnection(localhost);
