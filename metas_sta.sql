260d30421ea0a4036b40c9322ff6d3660c83923a
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2021-08-08 11:54:11 -0300

    Commit 1 do Klever

--- metas_sta.sql
+++ metas_sta.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 -- Banco de dados: `metas_sta`
 --
 
-CREATE DATABASE mydatabase CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE db_metas_sta CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
