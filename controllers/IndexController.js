fb96cd694f2effbadb331dd191d1143ca91a884b
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2021-08-09 22:41:38 -0300

    Adição da listagem das metas qualitativas

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -2,13 +2,13 @@ const express = require('express');
 const database = require('../database/database');
 
 const fisicas = (req, res) => {
-  database.connect(function(err) {
-  
+  database.connect(function (err) {
+
     database.query("SELECT CD_PROCESSO, DS_NOME FROM processos; SELECT p.CD_PROCESSO, s.DS_NOME, mf.NR_META, mf.NR_CONSTATACOES, mf.NR_EVID_APRESENTA, mf.DS_JUSTIFIC FROM metas_fisicas mf INNER JOIN setores s ON s.CD_SETOR = mf.CD_SETOR INNER JOIN processos p ON p.CD_PROCESSO = mf.CD_PROCESSO",
-      ( err, result, fields ) => {
+      (err, result, fields) => {
 
         result[0].forEach(value => {
-          value.id = value.DS_NOME.substr(0, 2);
+          value.id = value.DS_NOME.split(' ')[0];
         });
 
         res.render('metasfisicas/index', {
@@ -18,36 +18,28 @@ const fisicas = (req, res) => {
 
       }
     );
-  
+
   });
 };
 
 const qualitativas = (req, res) => {
-  database.connect(function(err) {
-  
-      database.query("SELECT CD_SETORES, DS_NOME FROM setores; SELECT * FROM metas_qualit INNER JOIN ",
-        ( err, result, fields ) => {
-  
-          result[0].forEach(value => {
-            value.id = value.DS_NOME.substr(0, 2);
-          });
-  
-          res.render('metasqualitativas/index', {
-            setors: result[0],
-            physics_goals: result[1]
-          });
-  
-        }
-      );
+  database.connect(function (err) {
 
-      database.query("SELECT p.CD_PROCESSO, s.DS_NOME, mf.NR_META, mf.NR_CONSTATACOES, mf.NR_EVID_APRESENTA, mf.DS_JUSTIFIC FROM metas_fisicas mf INNER JOIN setores s ON s.CD_SETOR = mf.CD_SETOR INNER JOIN processos p ON p.CD_PROCESSO = mf.CD_PROCESSO WHERE p.CD_PROCESSO = 1", function (err, result, fields) {
+    database.query("SELECT s.CD_SETOR, s.DS_NOME SETOR FROM metas_qualit mq INNER JOIN setores s ON mq.CD_SETOR = s.CD_SETOR GROUP BY mq.CD_SETOR; SELECT s.CD_SETOR, s.DS_NOME, mq.DS_NOME, mq.NR_CONSTATACOES, mq.NR_EVID_APRESENTA, mq.DS_JUSTIFIC FROM metas_qualit mq INNER JOIN setores s ON mq.CD_SETOR = s.CD_SETOR",
+      (err, result, fields) => {
 
-        res.render('metasqualitativas/index', {
-          result: result
+        result[0].forEach(value => {
+          value.id = value.SETOR.split(' ')[0];
         });
 
-      });
-  
+        res.render('metasqualitativas/index', {
+          setors: result[0],
+          qualitative_goals: result[1]
+        });
+
+      }
+    );
+
   });
 };
 
--- database/database.js
+++ database/database.js
@@ -1,29 +1,29 @@
-const mysql      = require('mysql');
+const mysql = require('mysql');
 
 const localhost = {
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : "db_metas_sta",
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: "db_metas_sta",
   multipleStatements: true
 };
 
 const sta_casa = {
-  host     : '10.10.10.5',
-  user     : 'root',
-  password : 'controle',
-  database : "db_metas_sta",
+  host: '10.10.10.5',
+  user: 'root',
+  password: 'controle',
+  database: "db_metas_sta",
   multipleStatements: true
 };
 
-var connection = mysql.createConnection(localhost);
- 
-connection.connect(function(err) {
+var connection = mysql.createConnection(sta_casa);
+
+connection.connect(function (err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
- 
+
   console.log('connected as id ' + connection.threadId);
 });
 
